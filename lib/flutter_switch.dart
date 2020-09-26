e9e7fcfe246f4ffa0861ea688dcc0ae98320d728
Nickr <nickrgamer04@gmail.com>
2020-09-26 21:07:09 +0800

    added comments and font weight property

--- lib/flutter_switch.dart
+++ lib/flutter_switch.dart
@@ -3,35 +3,161 @@ library flutter_switch;
 import 'package:flutter/material.dart';
 
 class FlutterSwitch extends StatefulWidget {
-  final bool value, showOnOff;
-  final ValueChanged<bool> onToggle;
-  final Color activeColor,
-      inactiveColor,
-      activeTextColor,
-      inactiveTextColor,
-      toggleColor;
-  final double width, height, toggleSize, valueFontSize, borderRadius, padding;
-  final String activeText, inactiveText;
+  /// Creates a material design switch.
+  ///
+  /// The following arguments are required:
+  ///
+  /// * [value] determines whether this switch is on or off.
+  /// * [onToggle] is called when the user toggles the switch on or off.
+  ///
 
-  const FlutterSwitch(
-      {Key key,
-      this.value,
-      this.onToggle,
-      this.activeColor = Colors.blue,
-      this.inactiveColor = Colors.grey,
-      this.activeTextColor = Colors.white70,
-      this.inactiveTextColor = Colors.white70,
-      this.toggleColor = Colors.white,
-      this.width = 70.0,
-      this.height = 35.0,
-      this.toggleSize = 25.0,
-      this.valueFontSize = 16.0,
-      this.borderRadius = 20.0,
-      this.padding = 4.0,
-      this.showOnOff = false,
-      this.activeText,
-      this.inactiveText})
-      : super(key: key);
+  const FlutterSwitch({
+    Key key,
+    @required this.value,
+    @required this.onToggle,
+    this.activeColor = Colors.blue,
+    this.inactiveColor = Colors.grey,
+    this.activeTextColor = Colors.white70,
+    this.inactiveTextColor = Colors.white70,
+    this.toggleColor = Colors.white,
+    this.width = 70.0,
+    this.height = 35.0,
+    this.toggleSize = 25.0,
+    this.valueFontSize = 16.0,
+    this.borderRadius = 20.0,
+    this.padding = 4.0,
+    this.showOnOff = false,
+    this.activeText,
+    this.inactiveText,
+    this.activeTextFontWeight,
+    this.inactiveTextFontWeight,
+  }) : super(key: key);
+
+  /// Determines if the switch is on or off.
+  ///
+  /// This property is required.
+  final bool value;
+
+  /// Called when the user toggles the switch.
+  ///
+  /// This property is required.
+  ///
+  /// [onToggle] should update the state of the parent [StatefulWidget]
+  /// using the [setState] method, so that the parent gets rebuilt; for example:
+  ///
+  /// ```dart
+  /// FlutterSwitch(
+  ///   value: _status,
+  ///   width: 110,
+  ///   borderRadius: 30.0,
+  ///   onToggle: (val) {
+  ///     setState(() {
+  ///        _status = val;
+  ///     });
+  ///   },
+  /// ),
+  /// ```
+  final ValueChanged<bool> onToggle;
+
+  /// Displays an on or off text.
+  ///
+  /// Text value can be override by the [activeText] and
+  /// [inactiveText] properties.
+  ///
+  /// Defaults to 'false' if no value was given.
+  final bool showOnOff;
+
+  /// The text to display when the switch is on.
+  /// This parameter is only necessary when [showOnOff] property is true.
+  ///
+  /// Defaults to 'On' if no value was given.
+  ///
+  /// To change value style, the following properties are available
+  ///
+  /// [activeTextColor] - The color to use on the text value when the switch is on.
+  /// [activeTextFontWeight] - The font weight to use on the text value when the switch is on.
+  final String activeText;
+
+  /// The text to display when the switch is off.
+  /// This parameter is only necessary when [showOnOff] property is true.
+  ///
+  /// Defaults to 'Off' if no value was given.
+  ///
+  /// To change value style, the following properties are available
+  ///
+  /// [inactiveTextColor] - The color to use on the text value when the switch is off.
+  /// [inactiveTextFontWeight] - The font weight to use on the text value when the switch is off.
+  final String inactiveText;
+
+  /// The color to use on the switch when the switch is on.
+  ///
+  /// Defaults to [Colors.blue].
+  final Color activeColor;
+
+  /// The color to use on the switch when the switch is off.
+  ///
+  /// Defaults to [Colors.grey].
+  final Color inactiveColor;
+
+  /// The color to use on the text value when the switch is on.
+  /// This parameter is only necessary when [showOnOff] property is true.
+  ///
+  /// Defaults to [Colors.white70].
+  final Color activeTextColor;
+
+  /// The color to use on the text value when the switch is off.
+  /// This parameter is only necessary when [showOnOff] property is true.
+  ///
+  /// Defaults to [Colors.white70].
+  final Color inactiveTextColor;
+
+  /// The font weight to use on the text value when the switch is on.
+  /// This parameter is only necessary when [showOnOff] property is true.
+  ///
+  /// Defaults to [FontWeight.w900].
+  final FontWeight activeTextFontWeight;
+
+  /// The font weight to use on the text value when the switch is off.
+  /// This parameter is only necessary when [showOnOff] property is true.
+  ///
+  /// Defaults to [FontWeight.w900].
+  final FontWeight inactiveTextFontWeight;
+
+  /// The color to use on the toggle of the switch.
+  ///
+  /// Defaults to [Colors.white].
+  final Color toggleColor;
+
+  /// The given width of the switch.
+  ///
+  /// Defaults to a width of 70.0.
+  final double width;
+
+  /// The given height of the switch.
+  ///
+  /// Defaults to a height of 35.0.
+  final double height;
+
+  /// The size of the toggle of the switch.
+  ///
+  /// Defaults to a size of 25.0.
+  final double toggleSize;
+
+  /// The font size of the values of the switch.
+  /// This parameter is only necessary when [showOnOff] property is true.
+  ///
+  /// Defaults to a size of 16.0.
+  final double valueFontSize;
+
+  /// The border radius of the switch.
+  ///
+  /// Defaults to the value of 20.0.
+  final double borderRadius;
+
+  /// The padding of the switch.
+  ///
+  /// Defaults to the value of 4.0.
+  final double padding;
 
   @override
   _FlutterSwitchState createState() => _FlutterSwitchState();
@@ -135,13 +261,21 @@ class _FlutterSwitchState extends State<FlutterSwitch>
     );
   }
 
+  FontWeight get _activeTextFontWeight => widget.activeTextFontWeight != null
+      ? widget.activeTextFontWeight
+      : FontWeight.w900;
+  FontWeight get _inactiveTextFontWeight =>
+      widget.inactiveTextFontWeight != null
+          ? widget.inactiveTextFontWeight
+          : FontWeight.w900;
+
   Widget get _activeText {
     if (widget.showOnOff) {
       return Text(
         (widget?.activeText != null) ? widget.activeText : "On",
         style: TextStyle(
           color: widget.activeTextColor,
-          fontWeight: FontWeight.w900,
+          fontWeight: _activeTextFontWeight,
           fontSize: widget.valueFontSize,
         ),
       );
@@ -156,9 +290,10 @@ class _FlutterSwitchState extends State<FlutterSwitch>
         (widget?.inactiveText != null) ? widget.inactiveText : "Off",
         style: TextStyle(
           color: widget.inactiveTextColor,
-          fontWeight: FontWeight.w900,
+          fontWeight: _inactiveTextFontWeight,
           fontSize: widget.valueFontSize,
         ),
+        textAlign: TextAlign.right,
       );
     }
 
