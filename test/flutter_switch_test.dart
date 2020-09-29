392d97f920da42f42674e92348e4bdcb5286495a
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2020-09-29 16:50:17 +0300

    Add tests and fix readability

--- test/flutter_switch_test.dart
+++ test/flutter_switch_test.dart
@@ -7,47 +7,82 @@ void main() {
   final alignToggleIndicatorFinder = find.byWidgetPredicate(
       (widget) => widget is Align && widget.child is Container);
 
-  testWidgets(
-    "displays the toggle indicator on the right if the given value is true",
-    (WidgetTester tester) async {
-      await tester.pumpWidget(
-        _FlutterSwitchTestbed(
-          value: true,
-        ),
-      );
+  group("FlutterSwitch", () {
+    testWidgets(
+      "displays the toggle indicator on the right if the given value is true",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          _FlutterSwitchTestbed(
+            value: true,
+          ),
+        );
 
-      final align = tester.widget<Align>(alignToggleIndicatorFinder);
+        final align = tester.widget<Align>(alignToggleIndicatorFinder);
 
-      expect(align.alignment, equals(Alignment.centerRight));
-    },
-  );
+        expect(align.alignment, equals(Alignment.centerRight));
+      },
+    );
 
-  testWidgets(
-    "displays the toggle indicator on the left if the given value is false",
-    (WidgetTester tester) async {
-      await tester.pumpWidget(
-        _FlutterSwitchTestbed(
-          value: false,
-        ),
-      );
+    testWidgets(
+      "displays the toggle indicator on the left if the given value is false",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          _FlutterSwitchTestbed(
+            value: false,
+          ),
+        );
 
-      final align = tester.widget<Align>(alignToggleIndicatorFinder);
+        final align = tester.widget<Align>(alignToggleIndicatorFinder);
 
-      expect(align.alignment, equals(Alignment.centerLeft));
-    },
-  );
+        expect(align.alignment, equals(Alignment.centerLeft));
+      },
+    );
+
+    testWidgets(
+      "changes the toggle indicator alignment to the right on tap when the given value is false",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          _FlutterSwitchTestbed(
+            value: false,
+          ),
+        );
+
+        await tester.tap(find.byType(GestureDetector));
+        await tester.pump();
+
+        final align = tester.widget<Align>(alignToggleIndicatorFinder);
+
+        expect(align.alignment, equals(Alignment.centerLeft));
+      },
+    );
+
+    testWidgets(
+      "changes the toggle indicator alignment to the left on tap when the given value is true",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          _FlutterSwitchTestbed(
+            value: true,
+          ),
+        );
+
+        await tester.tap(find.byType(GestureDetector));
+        await tester.pumpAndSettle();
+
+        final align = tester.widget<Align>(alignToggleIndicatorFinder);
+
+        expect(align.alignment, equals(Alignment.centerLeft));
+      },
+    );
+  });
 }
 
-/// A testbed needed to test the switch widget.
+/// A testbed class needed to test the [FlutterSwitch] widget.
 class _FlutterSwitchTestbed extends StatelessWidget {
   /// A callback that is called when the switch is toggled.
   final ValueChanged<bool> onToggle;
 
   /// An initial value of the switch.
   final bool value;
-
-  /// A default on toggle callback of the switch.
-  static void _defaultOnToggle(bool value) {}
 
   /// Creates a new instance of this testbed.
   const _FlutterSwitchTestbed({
@@ -62,9 +97,12 @@ class _FlutterSwitchTestbed extends StatelessWidget {
       home: Scaffold(
         body: FlutterSwitch(
           value: value,
-          onToggle: (value) => {},
+          onToggle: onToggle,
         ),
       ),
     );
   }
+
+  /// A default on toggle callback of the switch.
+  static void _defaultOnToggle(bool value) {}
 }
