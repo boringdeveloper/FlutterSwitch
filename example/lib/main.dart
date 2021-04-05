3fcf1cc4b58e44398b1955983ceb525b4fc7393f
zhupengtian <pengtian86@163.com>
2021-04-05 11:40:44 +0800

    add rounded rectangle switch

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,6 +28,8 @@ class _MyHomePageState extends State<MyHomePage> {
   bool status5 = false;
   bool status6 = false;
   bool status7 = false;
+  bool status8 = false;
+  bool status9 = false;
   bool isSwitchOn = false;
 
   Color _textColor = Colors.black;
@@ -297,6 +299,33 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
+                SizedBox(height: 10.0),
+                Text("Rounded Rectangle Switch"),
+                SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlutterSwitch(
+                      toggleRatio: 1.5,
+                      height: 40,
+                      width: 100,
+                      toggleSize: 40,
+                      value: status9,
+                      onToggle: (val) {
+                        setState(() {
+                          status9 = val;
+                        });
+                      },
+                    ),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "Value: $status9",
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
               ],
             ),
           ),
